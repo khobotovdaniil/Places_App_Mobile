3520ea3ad43ed23812cf81ccb9a72a048a2338ed
Daniil_Khobotov <dinich44@gmail.com>
2023-11-22 18:20:58 +0400

    add map

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import { StyleSheet } from 'react-native'
 import MapView, { Marker } from 'react-native-maps'
 
 export default function Map() {
+  const [selectedLocation, setSelectedLocation] = useState()
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
@@ -9,10 +12,28 @@ export default function Map() {
     longitudeDelta: 0.0421,
   }
 
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude
+    const lng = event.nativeEvent.coordinate.longitude
+
+    setSelectedLocation({ lat: lat, lng: lng })
+  }
+
   return (
     <MapView
       style={styles.map}
-      initialRegion={region}></MapView>
+      initialRegion={region}
+      onPress={selectLocationHandler}>
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
   )
 }
 
